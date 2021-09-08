0ad622f64a5c4029be27355e0818c8e91ed0d811
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-09-09 00:33:18 +0600

    final

--- src/components/SearchBar2/index.js
+++ src/components/SearchBar2/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHistory} from "react-router-dom";
 
-const SearchBar2 = () => {
+const SearchBar2 = ({setIsOpen}) => {
+    const history = useHistory()
+
+    const [inputValue, setInputValue] = useState("")
+    const input = (e) => {
+        setInputValue(e.target.value)
+    }
+    const handleClick = () => {
+        history.push(`/search/${inputValue}`)
+        setTimeout(() => setInputValue(""), 1000)
+    }
+    const handleClear = () => {
+
+        setIsOpen(false)
+    }
+    const enterPress = (e) => {
+        if (e.key === "Enter") {
+            handleClick()
+        }
+    }
     return (
-        <div>
-            
+        <div className="home__search-bar">
+            <input type="text" onKeyDown={enterPress} onChange={input} value={inputValue} className="home__input" placeholder="Найти фильм, сериал, персону......"/>
+            <button onClick={handleClick} disabled={!inputValue.trim()} className="home__search-btn">Search</button>
         </div>
     );
 };
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,31 +1,7 @@
-import {useHistory, useParams} from "react-router-dom";
-import {useState, useEffect} from "react";
-import axios from "axios";
+
+import SearchBar2 from "../../components/SearchBar2";
 
 const Home = () => {
-    const history = useHistory()
-    const [inputValue, setInputValue] = useState("")
-    const [page, setPage] = useState(1)
-    const [search, setSearch] = useState({})
-    const params = useParams()
-    useEffect(() => {
-        axios(`https://api.themoviedb.org/3/search/multi?query=${params.name}&page=${page}&language=ru&api_key=8bd09535ee32769ac84638efb6c3cfe5`)
-            .then(({data}) => {
-                setSearch(data)
-            })
-    }, [page, params.name])
-    const input = (e) => {
-        setInputValue(e.target.value)
-    }
-    const handleClick = () => {
-        history.push(`/search/${inputValue}`)
-        setTimeout(() => setInputValue(""), 1000)
-    }
-    const enterPress = (e) => {
-        if (e.key === "Enter") {
-            handleClick()
-        }
-    }
     return (
         <>
             <div className="home__backdrop"/>
@@ -33,10 +9,7 @@ const Home = () => {
                 <div className="container">
                     <h1 className="home__title">Добро пожаловать.</h1>
                     <h3 className="home__subtitle">Миллионы фильмов, сериалов и людей. Исследуйте сейчас.</h3>
-                    <div className="home__search-bar">
-                        <input type="text" onKeyDown={enterPress} onChange={input} value={inputValue} className="home__input" placeholder="Найти фильм, сериал, персону......"/>
-                        <button onClick={handleClick} disabled={!inputValue.trim()} className="home__search-btn">Search</button>
-                    </div>
+                 <SearchBar2 />
 
                 </div>
             </div>
